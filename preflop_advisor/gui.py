a3cf01d3fcf63976dd97f8e7c82a38dc7f6728fc
Johann <ksoeze.8v3v8@slmail.me>
2024-07-10 20:29:23 +0200

    Add Example Ranges Cleanup stuff for publish

--- preflop_advisor/gui.py
+++ preflop_advisor/gui.py
@@ -10,19 +10,9 @@ from preflop_advisor.position_selector import PositionSelector
 from preflop_advisor.outputframe import OutputFrame
 from preflop_advisor.randomizer import RandomButton
 
-SCREEN_SCRAPPER = True
-
 RANKS = list("AKQJT98765432")
 SUITS = list("cdhs")
 CARDS = list(rank + suit for suit in SUITS for rank in RANKS)
-
-from screen_scrapper.screen_scrapper_frame import ScreenScrapperFrame
-try:
-    from screen_scrapper.screen_scrapper_frame import ScreenScrapperFrame
-    #SCREEN_SCRAPPER = False
-except ImportError:
-    SCREEN_SCRAPPER = False
-
 
 class MainWindow:
     def __init__(self, root):
@@ -63,23 +53,11 @@ class MainWindow:
             self.configs["Output"],
             self.configs["TreeReader"])
 
-        if SCREEN_SCRAPPER:
-            #self.card_list_frame = tk.Frame(self.input_frame, pady=2)
-            self.card_list_frame = tk.Frame(self.root, pady=2)
-            self.player_list_frame = tk.Frame(self.root, pady=10)
-            self.screen_scrapper = ScreenScrapperFrame(
-                self.card_list_frame, self.player_list_frame, self.update_output_frame)
-            self.last_plo_tree = int(self.configs["TreeSelector"]["DefaultTree"])
-            self.last_plo5_tree =int(self.configs["TreeSelector"]["DefaultPLO5Tree"])
-            #print("Create ScreenScrapperFrame")
         self.grid_frames()
 
     def grid_frames(self):
         self.input_frame.grid(row=0, column=0, rowspan=2, sticky='N')
         self.output_frame.grid(row=0, column=1, padx=0, sticky='WN')
-        if SCREEN_SCRAPPER:
-            self.card_list_frame.grid(row=1, column=1, sticky='W')
-            #self.player_list_frame.grid(row=1, column=1, padx=10, pady=10)
 
         self.card_selector_position_frame.grid(row=0, column=0, sticky='W')
         self.tree_selector.grid(row=1, column=0, columnspan=2)
@@ -101,22 +79,6 @@ class MainWindow:
         if len(hand) == 4 and game == "NL" or len(hand) == 8 and game in ["PLO", "PLO8"] or len(hand) == 10 and game == "PLO5":
             self.output.update_output_frame(hand, position, tree_infos)
             return
-
-        if SCREEN_SCRAPPER:
-            hand = self.screen_scrapper.get_selected_hand()  # TODO ignores position for now
-            for i in range(0,len(hand),2):
-                if hand[i:i+2] not in CARDS:
-                    return 
-            if len(hand) == 8 and tree_infos["index"] >= int(self.configs["TreeSelector"]["DefaultPLO5Tree"]):
-                self.last_plo5_tree = tree_infos["index"]
-                self.tree_selector.set_other_tree(self.last_plo_tree)
-                self.update_card_and_position_selector(self.tree_selector.get_tree_infos())
-            if len(hand) == 10 and tree_infos["index"] < int(self.configs["TreeSelector"]["DefaultPLO5Tree"]):
-                self.last_plo_tree = tree_infos["index"]
-                self.tree_selector.set_other_tree(self.last_plo5_tree)
-                self.update_card_and_position_selector(self.tree_selector.get_tree_infos())
-            if len(hand) == 4 and game == "NL" or len(hand) == 8 or len(hand) == 10:
-                self.output.update_output_frame(hand, position, self.tree_selector.get_tree_infos())
 
     def update_card_and_position_selector(self, tree_infos):
         # select 4 or 2 cards in card_selector depending on game
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
+import os 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setuptools.setup(
     name='preflop_advisor',
@@ -8,7 +11,7 @@ setuptools.setup(
     author='ksoeze',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    
+    install_requires=required, 
     entry_points={
           'console_scripts': [
               'preflop_advisor = preflop_advisor.__main__:main'
